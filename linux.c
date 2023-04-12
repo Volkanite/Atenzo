3590f2b43239e3442575bfa71c1ca2788eec1e5b
Hakim Boyles <hakim@haktronic.com>
2023-04-11 20:03:14 -0400

    set baud rate

--- linux.c
+++ linux.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <termios.h>
 
 
 int fd = 0;
@@ -48,13 +49,56 @@ int main()
 		return 0;
 	}
 	
+	// Create new termios struct, we call it 'tty' for convention
+  	struct termios tty;
+
+  	// Read in existing settings, and handle any error
+  	if(tcgetattr(fd, &tty) != 0) {
+      printf("Error %i from tcgetattr: %s\n", errno, strerror(errno));
+      return 1;
+  	}
+
+  tty.c_cflag &= ~PARENB; // Clear parity bit, disabling parity (most common)
+  tty.c_cflag &= ~CSTOPB; // Clear stop field, only one stop bit used in communication (most common)
+  tty.c_cflag &= ~CSIZE; // Clear all bits that set the data size 
+  tty.c_cflag |= CS8; // 8 bits per byte (most common)
+  tty.c_cflag &= ~CRTSCTS; // Disable RTS/CTS hardware flow control (most common)
+  tty.c_cflag |= CREAD | CLOCAL; // Turn on READ & ignore ctrl lines (CLOCAL = 1)
+
+  tty.c_lflag &= ~ICANON;
+  tty.c_lflag &= ~ECHO; // Disable echo
+  tty.c_lflag &= ~ECHOE; // Disable erasure
+  tty.c_lflag &= ~ECHONL; // Disable new-line echo
+  tty.c_lflag &= ~ISIG; // Disable interpretation of INTR, QUIT and SUSP
+  tty.c_iflag &= ~(IXON | IXOFF | IXANY); // Turn off s/w flow ctrl
+  tty.c_iflag &= ~(IGNBRK|BRKINT|PARMRK|ISTRIP|INLCR|IGNCR|ICRNL); // Disable any special handling of received bytes
+
+  tty.c_oflag &= ~OPOST; // Prevent special interpretation of output bytes (e.g. newline chars)
+  tty.c_oflag &= ~ONLCR; // Prevent conversion of newline to carriage return/line feed
+
+  tty.c_cc[VTIME] = 10;    // Wait for up to 1s (10 deciseconds), returning as soon as any data is received.
+  tty.c_cc[VMIN] = 0;
+
+  	// Set in/out baud rate to be 115200
+  	cfsetispeed(&tty, B115200);
+  	cfsetospeed(&tty, B115200);
+
+  // Save tty settings, also checking for error
+  if (tcsetattr(fd, TCSANOW, &tty) != 0) {
+      printf("Error %i from tcsetattr: %s\n", errno, strerror(errno));
+      return 1;
+  }
+
+	
 	// Reboot device to flush any bad settings
-    //Send("ATWS\r");
+    Send("ATWS\r");
     //Send("ATZ\r");
+    sleep(1);
     
     Echo("ATL1\r"); // turn on line feed
-    
-	Echo("ATE0\r");//Echo off
+	//Echo("ATE0\r");//Echo off
+	Echo("ATE1\r");//Echo on
+	
 	Echo("ATI\r"); //identify
     Echo("STI\r"); //Print firmware ID string
     Echo("STDI\r"); //Print device hardware ID string
@@ -75,6 +119,26 @@ int main()
 	Send("ATTAAE\r"); //set tester address to hh
 	Send("STCFCPC\r");     //# clear all flow control address pairs
 	Send("STCFCPA 7E0 AE, 7E8 AE\r");   //# add flow control address pair
+	
+	//# message to check status of Brakes(last byte in response : 00 - brake is off, 01/02 - brake is on)
+	//BOO = Brake On/Off Switch
+	Echo("221101\r");
+
+	/*while (0)
+	{
+		Echo("221101\r");
+		Sleep(1000);
+	}*/
+	
+	
+	/*Send("27011\r"); //SecurityAccess ;)
+	int seed = GetSeed();
+	
+	//Reply with key
+	int key = GetKeyFromSeed(seed);
+	//BuildKeyMessage(buffer, key);
+	//Echo(buffer); //send key*/
+	
 	
 	return 0;
 }
