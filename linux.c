Volkanite/Atenzo

commit 47d438a1e6ffcc46a82a0fae7aa4537c72149d84

use DTC_CNT to know when there are DTCs

linux.c
@@ -339,7 +339,7 @@ int main()
             manualFanControl = 0;
             
         	//clear DTCs
-            if (Debug)
+            if (Debug && ParameterIds[DTC_CNT].Value == 1)
             {
                 //move(y-1, 0);
         	    //printw("Clearing DTCs..");

commit 51ea3913508dde8e75e1c5e3918c6a206198d027

fix bug introduced by 47d438a causing DTC not to be cleared

linux.c
@@ -336,19 +336,19 @@ int main()
         
         if (currentEngineState == 1 && previousEngineState == 0)
         {
-            manualFanControl = 0;
+            //Code in this block runs once every engine restart
             
-        	//clear DTCs
-            if (Debug && ParameterIds[DTC_CNT].Value == 1)
-            {
-                //move(y-1, 0);
-        	    //printw("Clearing DTCs..");
-        	    StatusPrint("Clearing DTCs..");
-    	        GetCommandResponse("14FF00\r", 0); //Clear DTCs
-            }
+            manualFanControl = 0;
         }
         
         previousEngineState = currentEngineState;
+        
+        //clear DTCs
+        if (Debug && ParameterIds[DTC_CNT].Value == 1)
+        {
+            StatusPrint("Clearing DTCs..");
+    	    GetCommandResponse("14FF00\r", 0); //Clear DTCs
+        }
         
         if ((ParameterIds[ECT].Value > tempHi || ParameterIds[TFT].Value > tempHi) && !ParameterIds[FAN].Value)
         {