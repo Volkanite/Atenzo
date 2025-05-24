aa9c0ee7e38f30cc45c23f669783b8ecc3acfcfa
Hakim Boyles <hakim@haktronic.com>
2025-05-24 18:39:51 -0400

    adjust LTFT alarm

--- config.h
+++ config.h
@@ -1,3 +1,8 @@
+#include "atenzo.h"
+
 #define FAN_CTRL_HI         95
 #define FAN_CTRL_LO         90
 #define ECT_TFT_TEMP_CRIT   100
+
+#define LTFT_CRIT           -10.00f
+#define ALARM_LTFT          TRUE
